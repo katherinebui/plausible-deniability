6678efefa54a9ba71bd9fbe65094f4b25194dc3d
Katherine Bui <kaatherine.buii@gmail.com>
2016-11-18 10:53:07 -0800

    Fix revisions to show new article_id and original_article_id

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,18 +33,10 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @revised_article = Article.new(article_params)
 
-    art_id = 
-
     if @revised_article.save
-      @revision = Revision.create(editor_id: current_user.id, article_id: art_id, original_article_id: @article.id)
+      @revision = Revision.create(editor_id: current_user.id, article_id: @revised_article.id, original_article_id: @article.id)
 
-      @article.revisions << @revision
-      
-      p '*********************************************'
-      p art_id
-      p @revision
-      p '*********************************************'
-      redirect_to article_revisions_path(@revision.id)
+      redirect_to article_revisions_path(@article)
     else
       render 'edit'
     end
@@ -60,12 +52,5 @@ class ArticlesController < ApplicationController
     def article_params
       params.require(:article).permit(:title, :body, :author_id)
     end
-
-    # polymorphic self ? as revised article???
-    # 
-
-    # def revision_params
-    #   params.require(:revision).permit(:editor_id, :article_id,:original_article_id)
-    # end
 
 end
